f14d38716d80fae46bad0fbd9ab10554d2ecfabc
your name here <nickbielak95@gmail.com>
2018-07-12 14:22:13 -0700

    Complete Phase 0"

--- music_app/app/controllers/application_controller.rb
+++ music_app/app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return nil unless session[:session_token]
-    @current_user = User.find_by(session_token: session[:session_token])
+    @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
-  def logged_in
+  def logged_in?
     !!current_user
   end
 
@@ -15,5 +15,10 @@ class ApplicationController < ActionController::Base
     @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
     session[:session_token] = @user.reset_session_token!
     redirect_to user_url(@user)
+  end
+
+  def logout_user!
+    current_user.reset_session_token!
+    session[:session_token] = nil
   end
 end
--- music_app/app/controllers/sessions_controller.rb
+++ music_app/app/controllers/sessions_controller.rb
@@ -5,9 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    login_user!
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+    if @user.nil?
+      flash.now[:errors] = ["Incorrect username/password"]
+      render :new
+    else
+      login_user!
+    end
   end
 
   def destroy
+      logout_user!
+      render :new
   end
 end
